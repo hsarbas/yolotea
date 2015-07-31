cdc287224f5cf3f2437aa8d22f0ee568fde6d632
hsarbas <harveyarbas@yahoo.com>
2015-07-31 16:32:34 +0800

    improved python GUI

--- 0.1/popups.py
+++ 0.1/popups.py
@@ -18,25 +18,23 @@ def popup_login(parent):
             self.frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
             self.labelframe1 = ttk.LabelFrame(self.frame, text='Login form')
-            self.labelframe1.pack(fill="both", expand="yes", padx=5, pady=5)
-            self.username_label = Label(self.labelframe1, text="Username")
+            self.labelframe1.pack(fill="both", expand="yes", padx=10, pady=10)
+            self.username_label = ttk.Label(self.labelframe1, text="Username")
             self.username_label.grid(row=0, column=0, padx=3, pady=5)
-            self.username_field = Entry(self.labelframe1)
+            self.username_field = ttk.Entry(self.labelframe1)
             self.username_field.focus_set()
             self.username_field.grid(row=0, column=1, padx=3, pady=5)
-            self.password_label = Label(self.labelframe1, text="Password")
+            self.password_label = ttk.Label(self.labelframe1, text="Password")
             self.password_label.grid(row=1, column=0, padx=3, pady=5)
-            self.password_field = Entry(self.labelframe1, show="*")
+            self.password_field = ttk.Entry(self.labelframe1, show="*")
             self.password_field.grid(row=1, column=1, padx=3, pady=5)
 
-
-            self.login_btn = Button(self.frame, text='Login', command=self.do)
-            self.login_btn.pack(pady=10)
+            self.login_btn = ttk.Button(self.frame, text='Login', command=self.do)
+            self.login_btn.pack(fill=X, padx=10, pady=10)
             self.top.bind("<Return>", self.do)
             self.top.grab_set()
 
         def do(self, event=None):
-            #self.value = self.splits_field.get()
 
             username = self.username_field.get()
             password = self.password_field.get()
@@ -46,7 +44,51 @@ def popup_login(parent):
                 self.top.grab_release()
                 self.top.destroy()
             else:
-                tm.showerror("Login error", "Incorrect username")
+                tm.showerror("Login error", "Incorrect username", parent=self.top)
+
+    pop = _popup()
+    parent.wait_window(pop.top)
+
+    value = pop.value
+    del pop
+
+    return value
+
+def popup_checkout(parent):
+    class _popup(object):
+
+        def __init__(self):
+            self.top = Toplevel(parent, takefocus=True)
+            self.top.resizable(0, 0)
+            self.top.title("Checkout Order")
+            self.value = None
+
+            self.frame = ttk.Frame(self.top)
+            self.frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+            self.labelframe1 = ttk.LabelFrame(self.frame, text='Checkout Order')
+            self.labelframe1.pack(fill="both", expand="yes", padx=5, pady=5)
+            self.customer_name_label = Label(self.labelframe1, text="Customer Name")
+            self.customer_name_label.grid(row=0, column=0, padx=3, pady=5)
+            self.customer_name_field = Entry(self.labelframe1)
+            self.customer_name_field.focus_set()
+            self.customer_name_field.grid(row=0, column=1, padx=3, pady=5)
+
+            self.submit_btn = Button(self.frame, text='Submit', command=self.do)
+            self.submit_btn.pack(pady=10)
+            self.top.bind("<Return>", self.do)
+            self.top.grab_set()
+
+        def do(self, event=None):
+            # self.value = self.splits_field.get()
+            customer_name = self.customer_name_field.get()
+
+            if len(customer_name) > 0:
+                tm.showinfo("Checkout Success", "Order successfully placed")
+                self.top.grab_release()
+                self.top.destroy()
+            else:
+                tm.showerror("Checkout Error", "Enter Customer Name", parent=self.top)
 
     pop = _popup()
     parent.wait_window(pop.top)
--- 0.1/take_order.py
+++ 0.1/take_order.py
@@ -55,7 +55,7 @@ class Yolotea(ttk.Frame):
         self.parent.config(menu=self.menubar)
 
     def init_paned_window(self):
-        #account labelframe
+        # account labelframe
         self.labelframe_account = ttk.LabelFrame(self, text="Account Details")
         self.labelframe_account.pack(side=TOP, fill=X, padx=10, pady=10)
         self.logo = ttk.Label(self.labelframe_account, text="YOLOTEA LOGO", width=20)
@@ -65,7 +65,7 @@ class Yolotea(ttk.Frame):
         self.employee_name.grid(row=0, column=4, padx=10, pady=5)
         self.employee_id.grid(row=1, column=4, padx=10, pady=5)
 
-        #main notebook
+        # main notebook
         self.notebook = ttk.Notebook(self)
         self.frame_milktea()
         self.frame_fruittea()
@@ -75,32 +75,131 @@ class Yolotea(ttk.Frame):
 
         self.notebook.pack(side=LEFT, fill=BOTH, expand=True, padx=10, pady=10)
 
-        #order details labelframe
-        self.labelframe_orderDetails = ttk.LabelFrame(self, text="Order Details")
-        self.labelframe_orderDetails.pack(side=RIGHT, fill=Y, padx=10, pady=10)
-        self.order_listbox = tk.Label(self.labelframe_orderDetails, height=28, width=40, bg="white")
+        # cart details labelframe
+        self.labelframe_orderDetails = ttk.LabelFrame(self, text="Cart Details")
+        self.labelframe_orderDetails.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        self.order_listbox = tk.Label(self.labelframe_orderDetails, height=28, width=60, bg="white")
         self.order_listbox.pack(side=TOP, padx=5, pady=5)
         self.total_label = ttk.Label(self.labelframe_orderDetails, text="Total Amount: P0.00")
         self.total_label.pack(side=TOP, fill=Y, pady=10)
-        self.checkout_btn = ttk.Button(self.labelframe_orderDetails, text='Checkout', command=None)
-        self.checkout_btn.pack(side=BOTTOM, padx=10, pady=10)
+        self.checkout_btn = ttk.Button(self.labelframe_orderDetails, text='Checkout Order', command=self.checkout)
+        self.checkout_btn.pack(side=BOTTOM, fill=BOTH, expand=TRUE, padx=10, pady=10)
+
+    def checkout(self):
+        value = popups.popup_checkout(self)
+
+    # order details labelframe for milktea and fruit tea
+    def labelframe_order_details_milktea_fruittea(self, f):
+        labelframe = ttk.LabelFrame(f, text="Order Details")
+        labelframe.pack(side=LEFT, fill=BOTH, expand=TRUE, padx=30, pady=10)
+
+        labelframe_size = ttk.LabelFrame(labelframe, text="Size")
+        labelframe_sugarlevel = ttk.LabelFrame(labelframe, text="Sugar Level")
+        labelframe_numdrink = ttk.LabelFrame(labelframe, text="Number of Drinks")
+        labelframe_sinkers = ttk.LabelFrame(labelframe, text="Sinkers")
+        btn_addtocart = ttk.Button(labelframe, text="Add Order to Cart", command=None)
+
+        labelframe_size.pack(side=TOP, fill=X, expand=TRUE, padx=10, pady=10)
+        labelframe_sugarlevel.pack(side=TOP, fill=X, expand=TRUE, padx=10, pady=10)
+        labelframe_numdrink.pack(side=TOP, fill=X, expand=TRUE, padx=10, pady=10)
+        labelframe_sinkers.pack(side=TOP, fill=X, expand=TRUE, padx=10, pady=10)
+        btn_addtocart.pack(side=TOP, fill=BOTH, expand=TRUE, padx=10, pady=10)
+
+        for x in range(0, 6):
+            Grid.grid_columnconfigure(labelframe_sinkers, x, weight=1)
+            Grid.grid_rowconfigure(labelframe_sinkers, x, weight=1)
+
+        radiobox_L = ttk.Radiobutton(labelframe_size, text="Large (L)", value=0, command=None)
+        radiobox_XL = ttk.Radiobutton(labelframe_size, text="Extra Large (XL)", value=1, command=None)
+        radiobox_L.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        radiobox_XL.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+
+        radiobox_full = ttk.Radiobutton(labelframe_sugarlevel, text="Full (100%)", value=2, command=None)
+        radiobox_half = ttk.Radiobutton(labelframe_sugarlevel, text="Half (50%)", value=3, command=None)
+        radiobox_none = ttk.Radiobutton(labelframe_sugarlevel, text="None (0%)", value=4, command=None)
+        radiobox_full.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        radiobox_half.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        radiobox_none.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+
+        label_quantity = ttk.Label(labelframe_numdrink, text="Quantity", anchor=tk.E)
+        entry_quantity = ttk.Entry(labelframe_numdrink)
+        label_quantity.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        entry_quantity.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+
+        checkbutton_popping_bobba = ttk.Checkbutton(labelframe_sinkers, text="Popping Bobba")
+        checkbutton_black_pearls = ttk.Checkbutton(labelframe_sinkers, text="Black Pearls")
+        checkbutton_nata_crystals = ttk.Checkbutton(labelframe_sinkers, text="Nata Crystals")
+        checkbutton_foam = ttk.Checkbutton(labelframe_sinkers, text="Foam")
+        checkbutton_panacotta = ttk.Checkbutton(labelframe_sinkers, text="Panacotta")
+        label_sinker_quantity_popping_bobba = ttk.Label(labelframe_sinkers, text="Quantity", anchor=tk.E)
+        label_sinker_quantity_black_pearls = ttk.Label(labelframe_sinkers, text="Quantity", anchor=tk.E)
+        label_sinker_quantity_nata_crystals = ttk.Label(labelframe_sinkers, text="Quantity", anchor=tk.E)
+        label_sinker_quantity_foam = ttk.Label(labelframe_sinkers, text="Quantity", anchor=tk.E)
+        label_sinker_quantity_panacotta = ttk.Label(labelframe_sinkers, text="Quantity", anchor=tk.E)
+
+        checkbutton_popping_bobba.grid(row=0, column=0, padx=10, pady=10, sticky=N+S+E+W)
+        checkbutton_black_pearls.grid(row=1, column=0, padx=10, pady=10, sticky=N+S+E+W)
+        checkbutton_nata_crystals.grid(row=2, column=0, padx=10, pady=10, sticky=N+S+E+W)
+        checkbutton_foam.grid(row=3, column=0, padx=10, pady=10, sticky=N+S+E+W)
+        checkbutton_panacotta.grid(row=4, column=0, padx=10, pady=10, sticky=N+S+E+W)
+        label_sinker_quantity_popping_bobba.grid(row=0, column=1, padx=10, pady=10, sticky=N+S+E+W)
+        label_sinker_quantity_black_pearls.grid(row=1, column=1, padx=10, pady=10, sticky=N+S+E+W)
+        label_sinker_quantity_nata_crystals.grid(row=2, column=1, padx=10, pady=10, sticky=N+S+E+W)
+        label_sinker_quantity_foam.grid(row=3, column=1, padx=10, pady=10, sticky=N+S+E+W)
+        label_sinker_quantity_panacotta.grid(row=4, column=1, padx=10, pady=10, sticky=N+S+E+W)
+
+        entry_popping_bobba = ttk.Entry(labelframe_sinkers)
+        entry_black_pearls = ttk.Entry(labelframe_sinkers)
+        entry_nata_crystals = ttk.Entry(labelframe_sinkers)
+        entry_foam = ttk.Entry(labelframe_sinkers)
+        entry_panacotta = ttk.Entry(labelframe_sinkers)
+        entry_popping_bobba.grid(row=0, column=2, padx=10, pady=10, sticky=N+S+E+W)
+        entry_black_pearls.grid(row=1, column=2, padx=10, pady=10, sticky=N+S+E+W)
+        entry_nata_crystals.grid(row=2, column=2, padx=10, pady=10, sticky=N+S+E+W)
+        entry_foam.grid(row=3, column=2, padx=10, pady=10, sticky=N+S+E+W)
+        entry_panacotta.grid(row=4, column=2, padx=10, pady=10, sticky=N+S+E+W)
+
+    # order details labelframe for hottea
+    def labelframe_order_details_hottea(self, f):
+        labelframe = ttk.LabelFrame(f, text="Order Details")
+        labelframe.pack(side=LEFT, fill=BOTH, expand=TRUE, padx=30, pady=10)
+
+        labelframe_size = ttk.LabelFrame(labelframe, text="Size")
+        labelframe_numdrink = ttk.LabelFrame(labelframe, text="Number of Drinks")
+        btn_addtocart = ttk.Button(labelframe, text="Add Order to Cart", command=None)
+        labelframe_size.pack(side=TOP, fill=BOTH, padx=10, pady=10)
+        labelframe_numdrink.pack(side=TOP, fill=BOTH, padx=10, pady=10)
+        btn_addtocart.pack(side=TOP, fill=BOTH, expand=TRUE, padx=10, pady=10)
+
+        radiobox_L = ttk.Radiobutton(labelframe_size, text="Large (L)", value=0, command=None)
+        radiobox_XL = ttk.Radiobutton(labelframe_size, text="Extra Large (XL)", value=1, command=None)
+        radiobox_L.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        radiobox_XL.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+
+        label_quantity = ttk.Label(labelframe_numdrink, text="Quantity", anchor=tk.E)
+        entry_quantity = ttk.Entry(labelframe_numdrink)
+        label_quantity.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        entry_quantity.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
 
     def frame_milktea(self):
         f = ttk.Frame(self.notebook)
 
-        #milktea flavors
+        # milktea flavors
         labelframe = ttk.LabelFrame(f, text="Flavors")
-        labelframe.pack(side=LEFT, fill=Y, padx=5, pady=5)
+        labelframe.pack(side=LEFT, fill=BOTH, padx=10, pady=10)
+        for x in range(0, 9):
+            Grid.grid_columnconfigure(labelframe, x, weight=1)
+            Grid.grid_rowconfigure(labelframe, x, weight=1)
 
-        btn = ttk.Button(labelframe, text='Yolavit', command=None)
-        btn2 = ttk.Button(labelframe, text='Yolowinter', command=None)
-        btn3 = ttk.Button(labelframe, text='YoloTaro', command=None)
-        btn4 = ttk.Button(labelframe, text='Yolaberry', command=None)
-        btn5 = ttk.Button(labelframe, text='Yolonilla', command=None)
-        btn6 = ttk.Button(labelframe, text='Yolomatcha', command=None)
-        btn7 = ttk.Button(labelframe, text='Carpe Diem', command=None)
-        btn8 = ttk.Button(labelframe, text='WYSIWYG', command=None)
-        btn9 = ttk.Button(labelframe, text="C'est la vie", command=None)
+        btn = ttk.Button(labelframe, text='  Yolavit  ', command=None)
+        btn2 = ttk.Button(labelframe, text='  Yolowinter  ', command=None)
+        btn3 = ttk.Button(labelframe, text='  YoloTaro  ', command=None)
+        btn4 = ttk.Button(labelframe, text='  Yolaberry  ', command=None)
+        btn5 = ttk.Button(labelframe, text='  Yolonilla  ', command=None)
+        btn6 = ttk.Button(labelframe, text='  Yolomatcha  ', command=None)
+        btn7 = ttk.Button(labelframe, text='  Carpe Diem  ', command=None)
+        btn8 = ttk.Button(labelframe, text='  WYSIWYG  ', command=None)
+        btn9 = ttk.Button(labelframe, text="  C'est la vie  ", command=None)
         btn10 = ttk.Button(labelframe, text='YoloChoco', command=None)
         btn11 = ttk.Button(labelframe, text='Jasmint', command=None)
         btn12 = ttk.Button(labelframe, text='YoloMocha', command=None)
@@ -110,43 +209,107 @@ class Yolotea(ttk.Frame):
         btn16 = ttk.Button(labelframe, text='YoloKkaido', command=None)
         btn17 = ttk.Button(labelframe, text='YoloChocoNana', command=None)
 
-        btn.grid(row=0, column=0, padx=5, pady=5)
-        btn2.grid(row=1, column=0, padx=5, pady=5)
-        btn3.grid(row=2, column=0, padx=5, pady=5)
-        btn4.grid(row=3, column=0, padx=5, pady=5)
-        btn5.grid(row=4, column=0, padx=5, pady=5)
-        btn6.grid(row=5, column=0, padx=5, pady=5)
-        btn7.grid(row=6, column=0, padx=5, pady=5)
-        btn8.grid(row=7, column=0, padx=5, pady=5)
-        btn9.grid(row=8, column=0, padx=5, pady=5)
-        btn10.grid(row=0, column=1, padx=5, pady=5)
-        btn11.grid(row=1, column=1, padx=5, pady=5)
-        btn12.grid(row=2, column=1, padx=5, pady=5)
-        btn13.grid(row=3, column=1, padx=5, pady=5)
-        btn14.grid(row=4, column=1, padx=5, pady=5)
-        btn15.grid(row=5, column=1, padx=5, pady=5)
-        btn16.grid(row=6, column=1, padx=5, pady=5)
-        btn17.grid(row=7, column=1, padx=5, pady=5)
+        btn.grid(row=0, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn2.grid(row=1, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn3.grid(row=2, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn4.grid(row=3, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn5.grid(row=4, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn6.grid(row=5, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn7.grid(row=6, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn8.grid(row=7, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn9.grid(row=8, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn10.grid(row=0, column=1, padx=5, pady=5, sticky=N+S+E+W)
+        btn11.grid(row=1, column=1, padx=5, pady=5, sticky=N+S+E+W)
+        btn12.grid(row=2, column=1, padx=5, pady=5, sticky=N+S+E+W)
+        btn13.grid(row=3, column=1, padx=5, pady=5, sticky=N+S+E+W)
+        btn14.grid(row=4, column=1, padx=5, pady=5, sticky=N+S+E+W)
+        btn15.grid(row=5, column=1, padx=5, pady=5, sticky=N+S+E+W)
+        btn16.grid(row=6, column=1, padx=5, pady=5, sticky=N+S+E+W)
+        btn17.grid(row=7, column=1, padx=5, pady=5, sticky=N+S+E+W)
 
-        labelframe2 = ttk.LabelFrame(f)
-        labelframe2.pack(side=RIGHT, fill=BOTH, expand=TRUE, padx=5,pady=5)
+        self.labelframe_order_details_milktea_fruittea(f)
         self.notebook.add(f, text='MilkTea')
-
 
     def frame_fruittea(self):
         f = ttk.Frame(self.notebook)
+
+        # fruittea flavors
+        labelframe = ttk.LabelFrame(f, text="Flavors")
+        labelframe.pack(side=LEFT, fill=Y, padx=5, pady=5)
+        for x in range(0, 5):
+            Grid.grid_columnconfigure(labelframe, x, weight=1)
+            Grid.grid_rowconfigure(labelframe, x, weight=1)
+
+        btn = ttk.Button(labelframe, text='    YoloBlue    ', command=None)
+        btn2 = ttk.Button(labelframe, text='    YoloApple    ', command=None)
+        btn3 = ttk.Button(labelframe, text='    YoLychee    ', command=None)
+        btn4 = ttk.Button(labelframe, text='    Hakuna Matata    ', command=None)
+        btn5 = ttk.Button(labelframe, text='    Eurika    ', command=None)
+
+        btn.grid(row=0, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn2.grid(row=1, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn3.grid(row=2, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn4.grid(row=3, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn5.grid(row=4, column=0, padx=5, pady=5, sticky=N+S+E+W)
+
+        self.labelframe_order_details_milktea_fruittea(f)
         self.notebook.add(f, text='FruitTea')
 
     def frame_hottea(self):
         f = ttk.Frame(self.notebook)
+
+        # hottea flavors
+        labelframe = ttk.LabelFrame(f, text="Flavors")
+        labelframe.pack(side=LEFT, fill=Y, padx=5, pady=5)
+        for x in range(0, 2):
+            Grid.grid_columnconfigure(labelframe, x, weight=1)
+            Grid.grid_rowconfigure(labelframe, x, weight=1)
+
+        btn = ttk.Button(labelframe, text='   Assam/ Jasmine   ', command=None)
+        btn2 = ttk.Button(labelframe, text='   Stash Tea Bag   ', command=None)
+
+        btn.grid(row=0, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn2.grid(row=1, column=0, padx=5, pady=5, sticky=N+S+E+W)
+
+        self.labelframe_order_details_milktea_fruittea(f)
         self.notebook.add(f, text='HotTea')
 
     def frame_snacks(self):
         f = ttk.Frame(self.notebook)
+
+        # snacks
+        labelframe = ttk.LabelFrame(f, text="Flavors")
+        labelframe.pack(side=LEFT, fill=Y, padx=5, pady=5)
+        for x in range(0, 2):
+            Grid.grid_columnconfigure(labelframe, x, weight=1)
+            Grid.grid_rowconfigure(labelframe, x, weight=1)
+
+        btn = ttk.Button(labelframe, text='    Belgian Waffle    ', command=None)
+        btn2 = ttk.Button(labelframe, text='    Nachos    ', command=None)
+
+        btn.grid(row=0, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn2.grid(row=1, column=0, padx=5, pady=5, sticky=N+S+E+W)
+
+        self.labelframe_order_details_milktea_fruittea(f)
         self.notebook.add(f, text='YoloSnacks')
 
     def frame_combos(self):
         f = ttk.Frame(self.notebook)
+
+        # combos
+        labelframe = ttk.LabelFrame(f, text="Flavors")
+        labelframe.pack(side=LEFT, fill=Y, padx=5, pady=5)
+        for x in range(0, 2):
+            Grid.grid_columnconfigure(labelframe, x, weight=1)
+            Grid.grid_rowconfigure(labelframe, x, weight=1)
+
+        btn = ttk.Button(labelframe, text='      XL + BW      ', command=None)
+        btn2 = ttk.Button(labelframe, text='      XL + Nachos      ', command=None)
+
+        btn.grid(row=0, column=0, padx=5, pady=5, sticky=N+S+E+W)
+        btn2.grid(row=1, column=0, padx=5, pady=5, sticky=N+S+E+W)
+
+        self.labelframe_order_details_milktea_fruittea(f)
         self.notebook.add(f, text='YoloCombos')
 
 def main():
